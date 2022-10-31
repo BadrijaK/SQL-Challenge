3bac60ef41c929fd0092a50c84e1628591d4b9d8
BadrijaK <badrijak@gmail.com>
2022-10-31 13:12:49 +0000

    variable change

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -12,8 +12,8 @@ WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
 SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name
-FROM dept_manager AS M
-JOIN departments AS D
+FROM dept_manager AS m
+JOIN departments AS d
 	ON m.dept_no = d.dept_no
 JOIN employees AS e 
 	ON e.emp_no = m.emp_no;
@@ -22,10 +22,10 @@ JOIN employees AS e
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-JOIN dept_emp AS junct 
-	ON e.emp_no = junct.emp_no
+JOIN dept_emp AS de 
+	ON e.emp_no = de.emp_no
 JOIN departments AS d 
-	ON junct.dept_no = d.dept_no;
+	ON de.dept_no = d.dept_no;
 
 
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
@@ -39,19 +39,19 @@ AND last_name LIKE 'B%';
 
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-JOIN dept_emp AS junct 
-	ON e.emp_no = junct.emp_no
+JOIN dept_emp AS de 
+	ON e.emp_no = de.emp_no
 JOIN departments AS d 
-	ON junct.dept_no = d.dept_no
+	ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales';
 
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees AS e
-JOIN dept_emp AS junct 
-	ON e.emp_no = junct.emp_no
+JOIN dept_emp AS de 
+	ON e.emp_no = de.emp_no
 JOIN departments AS d 
-	ON junct.dept_no = d.dept_no
+	ON de.dept_no = d.dept_no
 WHERE d.dept_name = 'Sales' 
 OR d.dept_name = 'Development'
 ORDER BY last_name;
